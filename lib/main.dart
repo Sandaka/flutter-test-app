f1243b16a3b2201dc2ad95c46a466afd152dd422
Sandaka <sandaka94@gmail.com>
2021-09-21 00:42:38 +0530

    AppBar design part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,66 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My First Flutter App"),
-          backgroundColor: const Color.fromRGBO(10, 222, 1223, 0.8),
-          // actions: <Widget>[
-          //   IconButton(
-          //     icon: const Icon(Icons.add_alert),
-          //     tooltip: 'Show Snackbar',
-          //     onPressed: () {
-          //       ScaffoldMessenger.of(context).showSnackBar(
-          //           const SnackBar(content: Text('This is a snackbar')));
-          //     },
-          //   ),
-          //   IconButton(
-          //     icon: const Icon(Icons.navigate_next),
-          //     tooltip: 'Go to the next page',
-          //     onPressed: () {
-          //       Navigator.push(context, MaterialPageRoute<void>(
-          //         builder: (BuildContext context) {
-          //           return Scaffold(
-          //             appBar: AppBar(
-          //               title: const Text('Next page'),
-          //             ),
-          //             body: const Center(
-          //               child: Text(
-          //                 'This is the next page',
-          //                 style: TextStyle(fontSize: 24),
-          //               ),
-          //             ),
-          //           );
-          //         },
-          //       ));
-          //     },
-          //   ),
-          // ],
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hi Flutter'),
+        leading: IconButton(
+          // ignore: prefer_const_constructors
+          icon: Icon(Icons.menu),
+          onPressed: () {},
         ),
-        body: const Center(
-          child: Text(
-            "Hello Flutter!",
-            style: TextStyle(fontSize: 30),
+        actions: [
+          IconButton(
+            // ignore: prefer_const_constructors
+            icon: Icon(Icons.search),
+            onPressed: () {},
           ),
+          IconButton(
+            // ignore: prefer_const_constructors
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        // ignore: prefer_const_constructors
+        // flexibleSpace: Icon(
+        //   Icons.photo_camera,
+        //   size: 75.0,
+        //   color: Colors.white70,
+        // ),
+        flexibleSpace: Image.asset(
+          "assets/back1.jpeg",
+          fit: BoxFit.cover,
         ),
       ),
     );
